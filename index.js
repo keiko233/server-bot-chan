7867a6cabdba7217e351cde5e2d8f7c57717a4f1
keiko233 <keiko23333@gmail.com>
2023-04-01 02:03:03 +0800

    feat: Add a command to get system information
    - Add a command to get system information
    - Refactor system information into a separate function

--- index.js
+++ index.js
@@ -4,29 +4,44 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = JSON.parse(fs.readFileSync('./config.json')).token;
 const bot = new TelegramBot(token, {polling: true});
 
+function getSystemInfo() {
+  const cpuLoad = (os.loadavg()[0] * 100).toFixed(2);
+  const totalMem = os.totalmem();
+  const freeMem = os.freemem();
+  const memUsage = ((1 - freeMem / totalMem) * 100).toFixed(2);
+  const cpuCount = os.cpus().length;
+  const usedMem = (totalMem - freeMem) / 1024 / 1024 / 1024;
+  const totalMemMB = totalMem / 1024 / 1024 / 1024;
+  const userInfo = os.userInfo();
+  const hostname = os.hostname();
+
+  return {
+    cpuLoad,
+    totalMem,
+    freeMem,
+    memUsage,
+    cpuCount,
+    usedMem,
+    totalMemMB,
+    userInfo,
+    hostname
+  };
+}
+
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, '可用指令：\n/getinfo - 汇报信息');
 });
 
 bot.onText(/\/getinfo/, (msg) => {
-  const chatId = msg.chat.id;
-  const cpuLoad = os.loadavg()[0];
-  const totalMem = os.totalmem();
-  const freeMem = os.freemem();
-  const memUsage = (1 - freeMem / totalMem).toFixed(2);
-  const cpuCount = os.cpus().length;
-  const usedMem = (totalMem - freeMem) / 1024 / 1024 / 1024;
-  const totalMemMB = totalMem / 1024 / 1024 / 1024;
-  const userInfo = os.userInfo();
-  const hostname = os.hostname();
-  
-  const infoMsg = `Hostname: ${hostname}
-CPU占用率: ${(cpuLoad * 100).toFixed(2)}%
-CPU核心数量: ${cpuCount}
-内存使用率: ${(memUsage * 100).toFixed(2)}%
-已用内存: ${usedMem.toFixed(2)} GB
-总计内存: ${totalMemMB.toFixed(2)} GB
-当前用户: ${userInfo.username}`;
+  const chatId = msg.chat.id
+  const info = getSystemInfo();
+  const infoMsg = `Hostname: ${info.hostname}
+CPU占用率: ${info.cpuLoad}%
+CPU核心数量: ${info.cpuCount}
+内存使用率: ${info.memUsage}%
+已用内存: ${info.usedMem.toFixed(2)} GB
+总计内存: ${info.totalMemMB.toFixed(2)} GB
+当前用户: ${info.userInfo.username}`;
   bot.sendMessage(chatId, infoMsg);
 });
